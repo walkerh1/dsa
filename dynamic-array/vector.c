efb025a01700af2e32960d98032f9dcbeda7c6f1
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-11-19 16:45:39 +1100

    add resizing ability when pushing and popping

--- dynamic-array/vector.c
+++ dynamic-array/vector.c
@@ -1,43 +1,53 @@
 #include <stdlib.h>
 #include "vector.h"
 
-#define INITIAL_CAPACITY 64
+#define INITIAL_CAP 64
 
 typedef struct VecRep {
-   ValueType *values;
-   int length;
-   int capacity;
+   ValueType *vals;
+   int len;
+   int cap;
 } VecRep;
 
 Vector create(ValueType type) {
    Vector v = (Vector)malloc(sizeof(VecRep));
    assert(v != NULL);
-   v->length = 0;
-   v->capacity = INITIAL_CAPACITY;
-   v->values = (ValueType*)malloc(sizeof(ValueType) * v->capacity);
-   assert(v->values != NULL);
+   v->len = 0;
+   v->cap = INITIAL_CAP;
+   v->vals = (ValueType*)malloc(sizeof(ValueType) * v->cap);
+   assert(v->vals != NULL);
    return v;
 }
 
 ValueType get(Vector vec, int idx) {
    assert(vec != NULL);
-   assert(idx >= 0 && idx < vec->length);
-   return vec->values[idx];
+   assert(idx >= 0 && idx < vec->len);
+   return vec->vals[idx];
 }
 
 ValueType set(Vector vec, ValueType val, int idx) {
    assert(vec != NULL);
-   assert(idx >= 0 && idx < vec->length);
-   vec->values[idx] = val;
+   assert(idx >= 0 && idx < vec->len);
+   vec->vals[idx] = val;
 }
 
-void push(Vector vec, ValueType val);
+void push(Vector vec, ValueType val) {
+   assert(vec != NULL);
+   vec->len++;
+   if (vec->len == vec->cap) {
+      changeCap(vec, 2 * vec->cap);
+   }
+   vec->vals[(vec->len)-1] = val;
+}
 
 ValueType pop(Vector vec) {
    assert(vec != NULL);
-   assert(vec->length > 0);
-   vec->length--;
-   return vec->values[vec->length];
+   assert(vec->len > 0);
+   vec->len--;
+   if (4 * vec->len > vec->cap) {
+      changeCap(vec, vec->cap / 2);
+   }
+   return vec->vals[vec->len];
 }
 
 void insertAt(Vector vec, ValueType val, int idx);
@@ -45,7 +55,18 @@ void insertAt(Vector vec, ValueType val, int idx);
 ValueType removeAt(int idx);
 
 int len(Vector vec) {
-   return vec->length;
+   return vec->len;
 }
 
-void destroy(Vector vec) {}
+void destroy(Vector vec) {
+   assert(vec != NULL);
+   free(vec->vals);
+   free(vec);
+}
+
+void changeCap(Vector vec, int newCap) {
+   ValueType *newVals = (ValueType*)realloc(vec->vals, sizeof(ValueType) * newCap);
+   assert(newVals != NULL);
+   vec->cap = newCap;
+   vec->vals = newVals;
+}
--- dynamic-array/vector.h
+++ dynamic-array/vector.h
@@ -1,13 +1,13 @@
 typedef struct VecRep *Vector;
 typedef int ValueType;
 
-// create new vetor that stores provided type
+// create new vector that stores elements of the specified type
 Vector create(ValueType type);
 
-// return the value at idx
+// return the value held at idx
 ValueType get(Vector vec, int idx);
 
-// set the element at idx equal to value
+// set the element stored at idx of vector equal to value
 ValueType set(Vector vec, ValueType val, int idx);
 
 // insert value at the end of the vector
@@ -16,13 +16,13 @@ void push(Vector vec, ValueType val);
 // remove value from the end of the vector
 ValueType pop(Vector vec);
 
-// insert value at the index specified and shift subsequent elemenets right by 1
+// insert value at the index specified and shift subsequent values right by 1
 void insertAt(Vector vec, ValueType val, int idx);
 
-// remove value at idx and shift subsequent values left by 1
+// remove and return value at idx and shift subsequent values left by 1
 ValueType removeAt(int idx);
 
-// length of vector
+// return length of vector
 int len(Vector vec);
 
 // free all the memory used by vector
